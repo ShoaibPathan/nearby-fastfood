d31d56754e9c6de0c08702de92fb2381ef0e25bb
Priscilla Ip <mcipswitch@Priscillas-MacBook-Pro.local>
2020-07-17 11:24:06 -0400

    Add mapview and tableview

--- NearbyFastFood/NearbyFastFood/ViewController.swift
+++ NearbyFastFood/NearbyFastFood/ViewController.swift
@@ -7,21 +7,41 @@
 //
 
 import UIKit
+import MapKit
 
 class ViewController: UIViewController {
     
     let segmentedControl: UISegmentedControl = {
         let sc = UISegmentedControl(items: ["Map", "List"])
-        
-        // Style
         sc.backgroundColor = #colorLiteral(red: 0.8784313725, green: 0.8823529412, blue: 0.8862745098, alpha: 1)
         sc.selectedSegmentTintColor = #colorLiteral(red: 0.2509803922, green: 0, blue: 0.5098039216, alpha: 1)
         sc.setTitleTextAttributes([NSAttributedString.Key.foregroundColor : #colorLiteral(red: 0.1176470588, green: 0.1529411765, blue: 0.1803921569, alpha: 1)], for: UIControl.State.normal)
         sc.setTitleTextAttributes([NSAttributedString.Key.foregroundColor : #colorLiteral(red: 0.9450980392, green: 0.9058823529, blue: 0.7137254902, alpha: 1)], for: UIControl.State.selected)
-        
         sc.selectedSegmentIndex = 0 // UserDefaults Preference
+        sc.addTarget(self, action: #selector(handleSegmentChange), for: .valueChanged)
         return sc
     }()
+    
+    let mapView: MKMapView = {
+        let map = MKMapView()
+        return map
+    }()
+    
+    let tableView: UITableView = {
+        let table = UITableView()
+        return table
+    }()
+    
+    @objc func handleSegmentChange(_ sender: UISegmentedControl) {
+        
+        if sender.selectedSegmentIndex == 0 {
+            mapView.alpha = 1
+            tableView.alpha = 0
+        } else {
+            mapView.alpha = 0
+            tableView.alpha = 1
+        }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -32,10 +52,10 @@ class ViewController: UIViewController {
         view.backgroundColor = .white
         navigationItem.title = "Fast Food Places"
         
-        
         //[redView, blueView].forEach { view.addSubview($0) }
-        
         view.addSubview(segmentedControl)
+        view.insertSubview(mapView, belowSubview: segmentedControl)
+        view.insertSubview(tableView, belowSubview: mapView)
         setupLayouts()
     }
     
@@ -45,6 +65,9 @@ class ViewController: UIViewController {
         segmentedControl.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 24).isActive = true        
         segmentedControl.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 72).isActive = true
         segmentedControl.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -72).isActive = true
+        
+        mapView.anchor(top: view.topAnchor, leading: view.leadingAnchor, bottom: view.bottomAnchor, trailing: view.trailingAnchor)
+        tableView.anchor(top: segmentedControl.bottomAnchor, leading: view.leadingAnchor, bottom: view.bottomAnchor, trailing: view.trailingAnchor, padding: .init(top: 24, left: 0, bottom: 0, right: 0))
     }
 
 
